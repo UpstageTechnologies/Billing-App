823ddef72a9533d573dcaf1f2b6d0cea35b4a118
Upstage <Upstage@Mac-mini.local>
2026-02-25 13:12:23 +0530

    delete cr seller

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ import CustomerDashboard from "./CustomerDashboard";
 import ShopCart from "./ShopCart";
 import MasterLogin from "./MasterLogin";
 import MasterRegister from "./MasterRegister";
-import CreateSeller from "./pages/CreateSeller";
 export default function App() {
   return (
     <Routes>
